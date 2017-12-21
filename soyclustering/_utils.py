93c32d68e26bb515424de4574e01b317950b021c
lovit <soy.lovit@gmail.com>
2017-12-21 20:33:06 +0900

    Add filtering arguments for proportion_keywords

--- soyclustering/_utils.py
+++ soyclustering/_utils.py
@@ -11,8 +11,12 @@ def check_sparsity(mat):
     n,m = mat.shape
     return sum(len(np.where(mat[c] != 0)[0]) for c in range(n)) / (n*m)
 
-def proportion_keywords(centers, labels, topk=200, index2word=None):
+def proportion_keywords(centers, labels, min_score=0.5, topk=200,
+                        candidates_topk=1000, index2word=None, passwords=None):
+    
     l1_normalize = lambda x:x/x.sum()
+    
+    index_type = passwords and isinstance(list(passwords)[0], int)
 
     n_clusters, n_features = centers.shape
     n_samples_in_cluster = np.bincount(labels, minlength=n_clusters)
@@ -33,11 +37,21 @@ def proportion_keywords(centers, labels, topk=200, index2word=None):
         p_prop = l1_normalize(centers[c])
 
         indices = np.where(p_prop > 0)[0]
+        indices = sorted(indices, key=lambda idx:-p_prop[idx])[:candidates_topk]
         scores = [(idx, p_prop[idx] / (p_prop[idx] + n_prop[idx])) for idx in indices]
-        scores = sorted(scores, key=lambda x:-x[1])[:topk]
+        scores = [t for t in scores if t[1] >= min_score]
+        scores = sorted(scores, key=lambda x:-x[1])
         keywords.append(scores)
-
+    
+    if passwords and index_type:
+        scores = [t for t in scores if t[0] in passwords]
+        
     if index2word is not None:
         keywords = [[(index2word[idx], score) for idx, score in keyword] for keyword in keywords]
+        if passwords and not index_type:
+            keywords = [t for t in keywords if t[0] in passwords]
+
+    if topk > 0:
+        keywords = [keyword[:topk] for keyword in keywords]
 
     return keywords
