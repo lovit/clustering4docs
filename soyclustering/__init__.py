e16d878ec936ed2189693ac20754cee61eec0ff6
lovit <soy.lovit@gmail.com>
2018-05-25 15:52:25 +0900

    -

--- soyclustering/__init__.py
+++ soyclustering/__init__.py
@@ -1,5 +1,8 @@
-__author__ = 'lovit'
+__title__ = 'soyclustering'
 __version__ = '0.0.1'
+__author__ = 'Lovit'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2017 Lovit'
 
 from ._kmeans import SphericalKMeans
 from ._utils import proportion_keywords
