6e810f86c6e37f5c620b6d82f75f696f98145960
lovit <soy.lovit@gmail.com>
2018-10-17 04:53:06 +0900

    Commit merge_close_clusters

--- soyclustering/__init__.py
+++ soyclustering/__init__.py
@@ -1,8 +1,9 @@
 __title__ = 'soyclustering'
-__version__ = '0.0.1'
+__version__ = '0.0.4'
 __author__ = 'Lovit'
 __license__ = 'GPL v3'
 __copyright__ = 'Copyright 2017 Lovit'
 
 from ._kmeans import SphericalKMeans
 from ._utils import proportion_keywords
+from ._postprocess import merge_close_clusters
