6cb6d2aef5cee35cd1f8fd501d4f88aae17c605a
lovit <soy.lovit@gmail.com>
2018-11-05 07:36:42 +0900

    Update

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.md", encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name="soyclustering",
-    version='0.0.1',
+    version='0.0.4',
     author='Lovit',
     author_email='soy.lovit@gmail.com',
     url='https://github.com/lovit/clustering4docs',
     description="Python library for document clustering",
-    long_description=long_description,
-    install_requires=["scikit-learn>=0.19.1"],
+    long_description="Python library for document clustering",
+    install_requires=["numpy>=1.1"],
     keywords = ['document clustering', 'clustering labeling'],
     packages=find_packages()
 )
