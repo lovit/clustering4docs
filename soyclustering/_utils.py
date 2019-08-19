84ecae60d894faf8ee2910ab9b9780f8a3f2d5e0
lovit <soy.lovit@gmail.com>
2019-08-19 18:37:35 +0900

    Add arguments to functions in utils.py

--- soyclustering/_utils.py
+++ soyclustering/_utils.py
@@ -2,11 +2,31 @@ import numpy as np
 from sklearn.utils.extmath import safe_sparse_dot
 
 def inner_product(X, Y):
-    """X: shape=(n,p)
-    Y: shape=(p,m)
-    It returns (n,m)"""
+    """
+    Arguments
+    --------
+    X, Y: numpy.ndarray or scipy.sparse.csr_matrix
+        One of both must be sparse matrix
+        shape of X = (n,p)
+        shape of Y = (p,m)
+
+    Returns
+    -------
+    Z : scipy.sparse.csr_matrix
+        shape of Z = (n,m)
+    """
+
     return safe_sparse_dot(X, Y, dense_output=False)
 
-def check_sparsity(mat):
-    n,m = mat.shape
-    return sum(len(np.where(mat[c] != 0)[0]) for c in range(n)) / (n*m)
+def check_sparsity(x):
+    """
+    Argument
+    --------
+    x : scipy.sparse.csr_matrix
+
+    Returns
+    -------
+    sparsity : float
+        1 - proportion of nonzero elements
+    """
+    return 1 - x.nnz / (x.shape[0] * x.shape[1])
