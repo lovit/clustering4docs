b136f64ca9dba9bfdfe31f123532b60d0fa4af7a
lovit <soy.lovit@gmail.com>
2019-08-19 18:46:52 +0900

    Version up. 0.1.0

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+import soyclustering
 from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="soyclustering",
-    version='0.0.4',
-    author='Lovit',
+    version=soyclustering.__version__,
+    author=soyclustering.__author__,
     author_email='soy.lovit@gmail.com',
     url='https://github.com/lovit/clustering4docs',
     description="Python library for document clustering",
-    long_description="Python library for document clustering",
+    long_description=long_description,
     install_requires=["numpy>=1.1"],
     keywords = ['document clustering', 'clustering labeling'],
     packages=find_packages()
